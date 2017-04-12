b95a96fcd6c990278a6340674ba045d11c6c912a
Graeme Bell <bell.graeme@gmail.com>
2017-04-12 12:42:52 +0100

    Can view name, population and capital city of country selected from drop-down

--- public/app.js
+++ public/app.js
@@ -1,6 +1,10 @@
+var countriesArray = [];
+
 var app = function(){
   var url = "https://restcountries.eu/rest/v2/all"
-  makeRequest(url, requestComplete)
+  makeRequest(url, requestComplete);
+ var countrySelected = document.querySelector('select')
+  countrySelected.onchange = handleSelectChange;
 }
 
 var requestComplete = function(){
@@ -8,17 +12,34 @@ var requestComplete = function(){
   var jsonString = this.responseText
   var countries = JSON.parse(jsonString)
   populateList(countries)
-}
+  countriesArray = countries
+};
+
+var handleSelectChange = function(){
+  var liName = document.querySelector('#country-name')
+  var liPopulation = document.querySelector('#country-population')
+  var liCapital = document.querySelector('#country-capital')
+  var result
+  countriesArray.forEach(function(country){
+    if(country.name === this.value){
+      result= country }  
+  }.bind(this))
+  liName.innerText = result.name
+  liPopulation.innerText = result.population
+  liCapital.innerText = result.capital
+};
 
 var populateList = function(countries){
   var dropDown = document.querySelector('#drop-down')
 
-  countries.forEach(function(country){
+  countries.forEach(function(country,index){
     var option = document.createElement('option')
     option.innerText = country.name
+    option.id = index
     dropDown.appendChild(option)
   })
 }
+
 
 var makeRequest = function(url, callback){
   var request = new XMLHttpRequest()
